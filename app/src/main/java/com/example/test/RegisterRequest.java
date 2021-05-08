d939ad9079e8233dc6b646475f1f1cbfd16fce22
Sihyeon <32366711+aristohyun@users.noreply.github.com>
2021-05-08 22:40:41 +0900

    Update RegisterRequest.java

--- app/src/main/java/com/example/test/RegisterRequest.java
+++ app/src/main/java/com/example/test/RegisterRequest.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class RegisterRequest extends StringRequest {
 
     // 서버 URL 설정 ( PHP 파일 연동 )
-    final static private String URL = "http://118.67.135.180/Register.php";
+    final static private String URL = "http://118.67.135.180/user/register.php";
     private Map<String, String> map;
 
 
